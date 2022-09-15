564d570d0d893b17cc55833619ac02363d227e6f
Soumya Ranjan Sahu <88780525+Soumyaranjan2021@users.noreply.github.com>
2022-09-15 23:17:26 +0530

    setup done

---
+++ src/screens/Home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text, StatusBar } from 'react-native';
+
+import Styles from './Styles';
+
+
+const Home = ({  }) => {
+  return (
+    <View>
+      <StatusBar backgroundColor={'transparent'} barStyle={'dark-content'} />
+      
+    </View>
+  )
+};
+
+
+export default Home;
+
---
+++ src/screens/Home/Styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+
+export default styles = StyleSheet.create({
+    main: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    container: {
+        
+    },
+    section: {
+
+    },
+});
+
